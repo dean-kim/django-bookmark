009b926977b4104c0fe1bde084c67664755c65f5
dean-kim <keunu07@gmail.com>
2017-04-24 15:19:13 +0900

    Python Study start

---
+++ ch2/bookmark/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import unicode_literals # Python 2.x 지원용
+
+# Create your models here.
+
+@python_2_unicode_compatible # Python 2.x 지원용
+class Bookmark(models.Model):
+    title = models.CharField(Max_length=100, blank=True, null=True)
+    url = models.URLField('url', unique=True)
+
+    def __str__(self):
+        return self.title
