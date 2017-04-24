2f9604c0dfdaddded3f7a69507c6f4a6c9451eb6
dean-kim <keunu07@gmail.com>
2017-04-24 16:16:32 +0900

    admin load succeed

--- ch2/bookmark/admin.py
+++ ch2/bookmark/admin.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from bookmark.models import Bookmark
 
 # Register your models here.
+
+# BookmarkAdmin 클래스는 Bookmark 클래스가 Admin 사이트에서 어떤 모습으로 보여줄지 정의하는 클래스
+class BookmarkAdmin(admin.ModelAdmin):
+    # Bookmark 내용을 보여줄 때 title과 url을 화면에 출력
+    list_display = ('title', 'url')
+
+# admin.site.register() 함수를 사용해서 Bookmark와 BookmarkAdmin 클래스를 등록
+admin.site.register(Bookmark, BookmarkAdmin)
--- ch2/bookmark/models.py
+++ ch2/bookmark/models.py
@@ -2,14 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.encoding import unicode_literals # Python 2.x 지원용
+# from django.utils.encoding import unicode_literals # Python 2.x 지원용
 
 # Create your models here.
 
-@python_2_unicode_compatible # Python 2.x 지원용
+# @python_2_unicode_compatible # Python 2.x 지원용
+
+# django.db.models.Model 클래스를 상속받아서 정의
 class Bookmark(models.Model):
-    title = models.CharField(Max_length=100, blank=True, null=True)
+    # title 컬럼은 공백값(blank)을 가질 수도 있고 값이 없을(null) 수도 있음
+    title = models.CharField(max_length=100, blank=True, null=True)
     url = models.URLField('url', unique=True)
 
+    # 객체를 문자열로 표현할 때 사용하는 함수
     def __str__(self):
         return self.title
