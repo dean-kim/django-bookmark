5bd0e5ef27ccfbc75b05cedb6ca76cfff5e76f17
dean-kim <keunu07@gmail.com>
2017-04-25 10:55:40 +0900

    templates ing

--- ch2/bookmark/views.py
+++ ch2/bookmark/views.py
@@ -3,4 +3,39 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+"""
+클래스형 제네릭 뷰를 사용하기 위해 ListView, DetailView를 import
+generic view?
+: 장고에서 기본적으로 제공하는 클래스, 웹 프로그램 개발 시 공통적으로 사용하는 로직을 미리 반영한 클래스임.
+  generic view를 상속받아서 필요한 속성과 메소드를 오버라이딩하여 사용할 수 있다.
+"""
+from django.views.generic import ListView, DetailView
+# 테이블 조회를 위해 모델 클래스를 import
+from bookmark.models import Bookmark
+
 # Create your views here.
+
+# ListView
+"""
+BookmarkLV는 테이블의 레코드 리스트를 보여주기 위한 뷰, ListView를 상속 받음.
+그리고 명시적으로 지정하지 않아도 장고에서 디폴트로 알아서 지정해주는 속성이 2개 있음.
+1. 컨텍스트 변수로 object_list를 사용
+2. 템플릿 파일을 '모델명소문자_list.html' 형식의 이름으로 지정
+   (이 경우라면 bookmark_list.html)
+"""
+class BookmarkLV(ListView):
+    model = Bookmark
+
+# DetailView
+"""
+BookmarkDV는 테이블의 특정 레코드에 대한 상세 정보를 보여주기 위한 뷰, DetailView를 상속 받음.
+그리고 명시적으로 지정하지 않아도 장고에서 디폴트로 알아서 지정해주는 속성이 2개 있음
+1. 컨텍스트 변수로 object를 사용
+2. 템플릿 파일을 '모델명소문자_detail.html' 형식의 이름으로 지정
+   (이 경우라면 bookmark_detail.html)
+DetailView를 상속받는 경우 특정 객체 하나를 컨텍스트 변수에 담아서 템플릿 시스템에 넘겨주면 됨.
+만일 테이블에서 기본 키로 조회해서 특정 객체를 가져오는 경우에는 테이블명 즉 모델 클래스명만 지정해주면 됨.
+조회 시 사용할 기본 키 값은 URLconf에서 추출해 뷰로 넘어온 파라미터를 사용함.
+"""
+class BookmarkDV(DetailView):
+    model = Bookmark
--- ch2/mysite/urls.py
+++ ch2/mysite/urls.py
@@ -13,9 +13,30 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+# 장고의 내장함수인 url() 함수 import
 from django.conf.urls import url
 from django.contrib import admin
 
+# URLconf에서 뷰를 호출하므로 뷰 모델의 관련 클래스를 import.
+from bookmark.views import BookmarkLV, BookmarkDV
+
+
+# url() 함수는 다음과 같이 5개의 인자를 가지고 있음 앞의 2개는 필수인자 뒤 3개는 선택적 인자임
+# url(regex, view, kwargs=None, name=None, prefix='')
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    """
+    admin site 관련 URLconf가 정의
+    URLconf를 다른 곳에서 정의한 URLconf를 가져와서 재활용하고자 할 때는 include() 함수를 사용.
+    다만 Admin site에 대한 URLconf인 admin.site.urls를 재활용할 때는 예외적으로 include()를 사용하지 않아도 가능
+    다음의 2가지 모두 가능함.
+    url(r'^admin/', admin.site.urls)
+    url(r'^admin/', include(admin.site.urls))
+    """
+    # Class-based views for Bookmark app
+
+    # 뷰를 클래스형 뷰로 정의하기 위해 각 URL에 따른 해당 클래스 및 as_view 메소드를 지정
+    # URL /bookmark/요청을 처리할 뷰 클래스를 BookmarkLV로 지정, URL 패턴의 이름은 'index'로 명명
+    url(r'^bookmark/$', BookmarkLV.as_view(), name='index'),
+    # URL /bookmark/숫자 요청을 처리할 뷰 클래스를 BookmarkDV로 지정, URL 패턴의 이름은 'detail'로 명
+    url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view(), name='detail'),
 ]
