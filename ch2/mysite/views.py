5ac64c81a18cb7bd016f2fb9a6b217a211a4b884
dean-kim <keunu07@gmail.com>
2017-06-05 10:18:49 +0900

    view.py update

--- ch2/mysite/views.py
+++ ch2/mysite/views.py
@@ -1,6 +1,21 @@
+# -*- coding: euckr -*-
+# coding=utf-8
 
 from django.views.generic.base import TemplateView
 
+#
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.core.urlresolvers import reverse_lazy
 
+
+#--- Homepage View
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+
+#--- User Creation
+class UserCreateView(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register_done')
