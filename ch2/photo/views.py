b2c4986226463cd62a298a4b1d188a0cf6ed84f8
dean-kim <keunu07@gmail.com>
2017-06-19 12:51:11 +0900

    photo views.py update

--- ch2/photo/views.py
+++ ch2/photo/views.py
@@ -1,6 +1,119 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+# 리다이렉트를 위한 단축 함수 redirect 임포트
+from django.shortcuts import render, redirect
+
+from django.views.generic import ListView, DetailView
+from photo.models import Album, Photo
+
+# forms.py에서 정의한 PhotoInlineFormSet 폼셋을 임포트
+# 폼셋이란 동일한 폼 여러 개로 구성됨 폼. 인라인 폼셋이란 메인 폼에 딸려 있는 폼셋을 말함, 테이블 간의 관계가
+# 1:N인 경우, N테이블의 레코드 여러 개를 한꺼번에 입력받기 위한 폼으로 사용
+from photo.forms import PhotoInlineFormSet
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from mysite.views import LoginRequiredMixin
 
 # Create your views here.
+
+class AlbumLV(ListView):
+    model = Album
+
+class AlbumDV(DetailView):
+    model = Album
+
+class PhotoDV(DetailView):
+    model = Photo
+
+#--- Add/Change/Update/Delete for Photo
+class PhotoCreateView(LoginRequiredMixin, CreateView):
+    model = Photo
+    fields = ['album', 'title', 'image', 'description']
+    success_url = reverse_lazy('photo:index')
+
+    def form_vaild(self, form):
+        form.instance.owner = self.request.user
+        return super(PhotoCreateView, self).form_valid(form)
+
+class PhotoChangeLV(LoginRequiredMixin, ListView):
+    model = Photo
+    template_name = 'photo/photo_change_list.html'
+
+    def get_queryset(self):
+        return Photo.objects.filter(owner=self.request.user)
+
+class PhotoUpdateView(LoginRequiredMixin, UpdateView):
+    model = Photo
+    fields = ['album', 'title', 'image', 'description']
+    success_url = reverse_lazy('photo:index')
+
+class PhotoDeleteView(LoginRequiredMixin, DetailView):
+    model = Photo
+    success_url = reverse_lazy('photo:index')
+
+#--- Add/Change/Update/Delete for Album
+#--- Change/Delete for Album
+class AlbumChangeLV(LoginRequiredMixin, ListView):
+    template_name = 'photo/album_change_list.html'
+
+    def get_queryset(self):
+        return Album.objects.filter(owner=self.request.user)
+
+class AlbumDeleteView(LoginRequiredMixin, ListView):
+    model = Album
+    success_url = reverse_lazy('photo:index')
+
+#--- InlineFormSet View
+#--- Add/Update for Album
+class AlbumPhotoCV(LoginRequiredMixin, CreateView):
+    model = Album
+    fields = ['name', 'description']
+    template_name = 'photo/album_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AlbumPhotoCV, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = PhotoInlineFormSet(self.request.POST, self.request.FILES)
+        else:
+            context['formset'] = PhotoInlineFormSet()
+        return context
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        context = self.get_context_data()
+        formset = context['formset']
+        for photoform in formset:
+            photoform_instance.owner = self.request.user
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return redirect(self.object.get_absolute_url())
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
+
+class AlbumPhotoUV(LoginRequiredMixin, UpdateView):
+    model = Album
+    fields = ['name', 'description']
+    template_name = 'photo/album_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AlbumPhotoUV, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['formset'] = PhotoInlineFormSet(self.request.POST, self.request.FILES, instance=self.object)
+        else:
+            context['formset'] = PhotoInlineFormSet(instance=self.object)
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+        if formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return redirect(self.object.get_absolute_url())
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
